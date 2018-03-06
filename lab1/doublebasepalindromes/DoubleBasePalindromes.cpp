e325fdadfa814cc215c57a5872d61edfba226705
Kamila <owsiankakamila@gmail.com>
2018-03-06 16:22:52 +0100

    doublebasepalindromes correct versio

--- lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -3,3 +3,77 @@
 //
 
 #include "DoubleBasePalindromes.h"
+
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive)
+{
+    uint64_t sum =0;
+    int i =1;
+    int numb=0;
+
+    numb = CreatePal(i,true);
+    while (numb < max_vaule_exculsive){
+        if (IsDoubleBasePal(numb))
+            sum+=numb;
+        i++;
+        numb = CreatePal(i,true);
+    }
+
+    i=1;
+
+    numb = CreatePal(i,false);
+    while (numb < max_vaule_exculsive){
+        if (IsDoubleBasePal(numb))
+            sum+=numb;
+        i++;
+        numb = CreatePal(i,false);
+    }
+
+
+    return sum;
+}
+int CreatePal(int numb, bool even){
+    int zero=0;
+    int add=0;
+    int temp =numb;
+    if (even){
+        add=temp%10;
+        zero++;
+    }
+    temp=temp/10;
+    while(temp){
+        add=10*add +temp%10;
+        temp = temp/10;
+        zero++;
+    }
+
+
+    numb=numb*pow(10,zero)+add; //czy kiedykolwiek nie bedzie rowne 0?
+    return numb;
+}
+
+bool IsDoubleBasePal (int numb){
+    int r,j;
+    bool is_pal = true;
+    int numb_bin[100];
+    int i=0;
+
+    // change to 2- base
+    while(numb){
+        r=numb%2;
+        numb=numb/2;
+        numb_bin[i] = r;
+        i++;
+    }
+
+    i--;
+
+    //check if is palindrome
+    for(j=0,i;(i>j) && (is_pal);j++,i--){
+        if (numb_bin[i] != numb_bin[j]) {
+            is_pal = false;
+        }
+    }
+
+    return is_pal;
+
+}
--- lab1/doublebasepalindromes/main.cpp
+++ lab1/doublebasepalindromes/main.cpp
@@ -1,75 +1,11 @@
 //
 // Created by kamila on 04.03.18.
 //
-
-#include <iostream>
-#include <cstdint>
-#include <vector>
-
-uint64_t DoubleBasePalindromes(int max_vaule_exculsive);
-int CreatePal(int numb, bool even);
-bool IsDoubleBasePal (int numb);
-
+#include "DoubleBasePalindromes.h"
 
 int main() {
-    if (IsDoubleBasePal(0)) std::cout << "TAK!" << std::endl;
+    std::cout << DoubleBasePalindromes(14560);
+    
 
     return 0;
-}
-
-uint64_t DoubleBasePalindromes(int max_vaule_exculsive)
-{
-    uint64_t sum =0;
-    int i =1;
-    bool flag =true;
-    int numb=0;
-
-    while(flag &&(numb<=max_vaule_exculsive)){
-        flag = false;
-
-
-
-
-    }
-}
-int CreatePal(int numb, bool even){
-    int zero=0;
-    int add=0;
-    int temp =numb;
-    if (even){
-        add=temp%10;
-        temp=temp/10;
-        zero++;
-    }
-    while(temp){
-        add=10*add +temp%10;
-        temp = temp/10;
-        zero++;
-    }
-    if (zero!=0) numb=numb*(10*zero)+add; //czy kiedykolwiek nie bedzie rowne 0?
-    return numb;
-}
-
-bool IsDoubleBasePal (int numb){
-    int r,j;
-    bool is_pal = true;
-    std::vector < int > numb_bin;
-
-    while(numb){
-
-        r=numb%2;
-        numb=numb/2;
-        numb_bin.push_back(r);
-    }
-
-    int i=numb_bin.size();
-    for(i,j=0;i<j,is_pal;i--,j++){
-        if (numb_bin[i] != numb_bin[j]) {
-            is_pal = false;
-        }
-    }
-
-
-    return is_pal;
-
 }
