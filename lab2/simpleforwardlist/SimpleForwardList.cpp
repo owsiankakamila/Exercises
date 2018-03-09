885a81cb06d2a5f3dfeb2346f82699bb6dcc6d7b
Kamila <owsiankakamila@gmail.com>
2018-03-09 09:49:12 +0100

    proba

--- lab2/simpleforwardlist/SimpleForwardList.cpp
+++ lab2/simpleforwardlist/SimpleForwardList.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "SimpleForwardList.h"
+
+
+
+ForwardList *CreateNode(int value){
+    ForwardList *head= new ForwardList;
+    head->value=value;
+    head->next= nullptr;
+   return head;
+}
+void DestroyList(ForwardList *list){
+    ForwardList *ptr= list->next;
+    delete list;
+    while (ptr != nullptr){
+        //delete
+        ptr = ptr->next;
+    }
+
+
+}
+ForwardList *PushFront(ForwardList *list, int value){
+
+}
+void Append(ForwardList *list, ForwardList *tail){
+
+}
--- lab2/simpleforwardlist/SimpleForwardList.h
+++ lab2/simpleforwardlist/SimpleForwardList.h
@@ -5,10 +5,14 @@
 #ifndef JIMP_EXERCISES_SIMPLEFORWARDLIST_H
 #define JIMP_EXERCISES_SIMPLEFORWARDLIST_H
 
+typedef struct Node{
+    int value;
+    struct ForwardList * next;
+}ForwardList;
 
-class SimpleForwardList {
-
-};
-
+ForwardList *CreateNode(int value);
+void DestroyList(ForwardList *list);
+ForwardList *PushFront(ForwardList *list, int value);
+void Append(ForwardList *list, ForwardList *tail);
 
 #endif //JIMP_EXERCISES_SIMPLEFORWARDLIST_H
