ccceed73832c9f705c62d6b99ca95f5f38ac1bc3
Kamila <owsiankakamila@gmail.com>
2018-04-10 20:44:51 +0200

    lab5: Matrix directories

---
+++ lab5/matrix/Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kamila on 10.04.18.
+//
+
+#include "Matrix.h"
---
+++ lab5/matrix/Matrix.h
@@ -0,0 +1,16 @@
+//
+// Created by kamila on 10.04.18.
+//
+
+#ifndef JIMP_EXERCISES_MATRIX_H
+#define JIMP_EXERCISES_MATRIX_H
+
+namespace algebra {
+    class Matrix {
+
+    };
+}
+
+
+
+#endif //JIMP_EXERCISES_MATRIX_H
