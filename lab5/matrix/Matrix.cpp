ea83fc151e392710a2a99205c37df9b0bc9c23ae
Kamila <owsiankakamila@gmail.com>
2018-04-11 21:17:50 +0200

    lab5: Matrix (definitions and few declarations)

--- lab5/matrix/Matrix.cpp
+++ lab5/matrix/Matrix.cpp
@@ -2,4 +2,32 @@
 // Created by kamila on 10.04.18.
 //
 
+#include <utility>
+
+#include <complex>
+#include <complex.h>
 #include "Matrix.h"
+
+algebra::Matrix::Matrix(int cols, int rows) {
+    cols_ = cols;
+    rows_ = rows;
+}
+
+algebra::Matrix::Matrix() {
+    cols_ = 0;
+    rows_ = 0;
+
+}
+
+algebra::Matrix::Matrix(const std::initializer_list<std::vector<std::complex<double>>> &elements): mat_{elements} {
+    // size of elements = cols_
+    cols_=mat_.size();
+    // size of elements[i] = rows_
+    rows_=elements.size();
+
+}
+
+std::pair<size_t, size_t > algebra::Matrix::Size() const {
+    return {rows_,cols_};
+}
+
--- lab5/matrix/Matrix.h
+++ lab5/matrix/Matrix.h
@@ -5,8 +5,59 @@
 #ifndef JIMP_EXERCISES_MATRIX_H
 #define JIMP_EXERCISES_MATRIX_H
 
+#include <vector>
+
 namespace algebra {
     class Matrix {
+    public:
+        // param constructor
+        Matrix (int cols, int rows);
+        // default constructor
+        Matrix ();
+        //Matlab constructor
+        Matrix (const std::string &str);
+        //initializer list constructor
+        Matrix (const std::initializer_list<std::vector<std::complex<double>>> &elements);
+
+
+
+        //copy constructor
+        Matrix(const Matrix &matrix);
+        //copy assignment??
+        Matrix &operator=(const Matrix &matrix);
+
+        //get size
+        std::pair<size_t , size_t > Size()const;
+        // print
+        std::string Print() const;
+        //pop elemnet
+        std::complex<double> Pop(int cols, int rows)const;
+
+
+        //set element // bool?
+        void Set(int cols, int rows, std::complex<double> val);
+
+        // +
+        Matrix Add(const Matrix &mat2)const;//?
+        // -
+        Matrix Sub(const Matrix &mat2);
+        // *
+        Matrix Mul(Matrix mat2);
+        Matrix Mul(int x);
+        // :
+        Matrix Div( Matrix mat2);
+        Matrix Div(int x);
+        // ^
+        Matrix Pow(int x);
+
+
+
+    private:
+        std::vector<std::vector<std::complex<double>>> mat_; //size row
+
+
+        int rows_;
+        int cols_;
 
     };
 }
