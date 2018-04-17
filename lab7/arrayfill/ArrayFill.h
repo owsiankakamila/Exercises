ba21ce6becad5fecd627ab69159db78007570131
Kamila <owsiankakamila@gmail.com>
2018-04-17 21:48:24 +0200

    IncrementedFill

--- lab7/arrayfill/ArrayFill.h
+++ lab7/arrayfill/ArrayFill.h
@@ -5,10 +5,106 @@
 #ifndef JIMP_EXERCISES_ARRAYFILL_H
 #define JIMP_EXERCISES_ARRAYFILL_H
 
+#include <iostream>
+#include <vector>
+#include <random>
 
-class ArrayFill {
+namespace arrays{
 
-};
+    //-------------------------------------------------------
+    class ArrayFill {
+    public:
+        virtual int Value(int index) const =0;
+    };
+
+    void FillArray(size_t size, const ArrayFill &filler, std::vector<int> *v) {
+        v->clear();
+        v->reserve(size);
+        for (size_t i = 0; i < size; i++) {
+            v->emplace_back(filler.Value(i));
+        }
+    }
+
+    //----------------------
+    class UniformFill : public ArrayFill {
+    public:
+        UniformFill(int value = 0) : value_{value} {}
+        virtual int Value(int index) const override;
+    private:
+        int value_;
+    };
+
+
+
+    int UniformFill::Value(int index) const {
+        return value_;
+    }
+    //--------------------------------
+
+
+    class IncrementalFill : public ArrayFill {
+    public:
+        IncrementalFill(int start, int step = 1) : start_{start}, step_{step} {}
+        virtual int Value(int index) const override;
+    private:
+        int start_;
+        int step_;
+    };
+
+
+    int IncrementalFill::Value(int index) const {
+        return (start_+(index*step_));
+    }
+
+    //----------------------------
+
+
+    class RandomFill : public ArrayFill {
+    public:
+        RandomFill() {}
+        virtual int Value(int index) const override;
+    private:
+        int value_;
+    };
+// generator, distribiution????
+
+
+    int RandomFill::Value(int index) const {
+
+    }
+    //--------------------------------
+
+    class SquaredFill : public ArrayFill {
+    public:
+        SquaredFill(int value = 0) : value_{value} {}
+        virtual int Value(int index) const override;
+    private:
+        int value_;
+    };
+
+
+
+    int SquaredFill::Value(int index) const {
+        return value_;
+    }
+    //--------------------------------
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #endif //JIMP_EXERCISES_ARRAYFILL_H
--- lab7/arrayfill/main.cpp
+++ lab7/arrayfill/main.cpp
@@ -2,3 +2,12 @@
 // Created by kamila on 17.04.18.
 //
 
+#include "ArrayFill.h"
+#include <iostream>
+
+int main() {
+    std::vector<int> vs;
+    arrays::FillArray(1024, arrays::UniformFill {77}, &vs);
+    return 0;
+}
+
