27e78e81630419ba8413b5ac54576c4910c90b80
Kamila <owsiankakamila@gmail.com>
2018-04-27 19:15:27 +0200

    IterableIterator, ZipperIterator, Wrapper

--- lab7/iterable/Iterable.cpp
+++ lab7/iterable/Iterable.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "Iterable.h"
+
+utility::ZipperIterator::ZipperIterator(std::vector<int>::const_iterator left_begin,
+                                        std::vector<std::string>::const_iterator right_begin,
+                                        std::vector<int>::const_iterator left_end,
+                                        std::vector<std::string>::const_iterator right_end) {
+    left_begin_=left_begin;
+    right_begin_ = right_begin;
+    left_end_=left_end;
+    right_end_=right_end;
+
+}
+
+std::pair<int, std::string> utility::ZipperIterator::Dereference() const {
+
+    return {*left_begin_,*right_begin_};
+}
+
+utility::IterableIterator &utility::ZipperIterator::Next() {
+    ++left_begin_;
+    ++right_begin_;
+    return *this;
+}
+
+bool utility::ZipperIterator::NotEquals(const std::unique_ptr<utility::IterableIterator> &other)const {
+    //niekoniecznie ale test ok
+    return Dereference()!=other->Dereference();
+
+
+
+}
+
+
+utility::IterableIteratorWrapper::IterableIteratorWrapper(std::unique_ptr<utility::IterableIterator> iterator) {
+    iterator_=move(iterator);
+
+}
+
+bool utility::IterableIteratorWrapper::operator!=(const utility::IterableIteratorWrapper &other) const {
+    return iterator_->NotEquals(other.iterator_);
+}
+
+std::pair<int, std::string> utility::IterableIteratorWrapper::operator*() const {
+    return iterator_->Dereference();
+}
+
+utility::IterableIteratorWrapper &utility::IterableIteratorWrapper::operator++() {
+    iterator_->Next();
+    return *this;
+}
--- lab7/iterable/Iterable.h
+++ lab7/iterable/Iterable.h
@@ -7,14 +7,77 @@
 
 #include <vector>
 #include <iostream>
+#include <memory>
 
 namespace utility{
-    class Zipper{
-    public:
-        std::vector<int>
 
+
+    class IterableIterator {
+    public:
+        virtual std::pair<int, std::string> Dereference() const=0;
+        //=*it
+
+        virtual IterableIterator &Next()=0;
+        //++it
+
+        virtual bool NotEquals(const std::unique_ptr<IterableIterator> &other) const=0;
+
+        ~IterableIterator ()= default;
+
+    private:
 
     };
+
+    class ZipperIterator: public IterableIterator{
+    public:
+        explicit ZipperIterator(std::vector<int>::const_iterator left_begin,
+                                std::vector<std::string>::const_iterator right_begin,
+                                std::vector<int>::const_iterator left_end,
+                                std::vector<std::string>::const_iterator right_end);
+
+        virtual std::pair<int, std::string> Dereference() const override ;
+
+        virtual IterableIterator &Next() override ;
+
+        virtual bool NotEquals(const std::unique_ptr<IterableIterator> &other)const override ;
+
+        ~ZipperIterator()= default;
+
+
+    private:
+        std::vector<int>::const_iterator left_begin_;
+        std::vector<std::string>::const_iterator right_begin_;
+        std::vector<int>::const_iterator left_end_;
+        std::vector<std::string>::const_iterator right_end_;
+    };
+
+
+    class IterableIteratorWrapper{//opakowuje
+    public:
+
+        IterableIteratorWrapper(std::unique_ptr<IterableIterator> iterator);
+        bool operator!=(const IterableIteratorWrapper &other)const;
+
+        std::pair<int, std::string> operator*() const;
+        IterableIteratorWrapper &operator++();
+
+        std::unique_ptr<utility::IterableIterator> iterator_;
+    };
+
+
+
+
+
+
+
+    class Zipper{
+    public:
+
+
+    private:
+    };
+
+
     class Enumerate {
         Enumerate(){}
 
