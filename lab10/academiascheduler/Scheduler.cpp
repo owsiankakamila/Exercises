028aba76ab8483573e8dabcdc5b31406d9fa88c5
Kamila <owsiankakamila@gmail.com>
2018-05-18 21:09:40 +0200

    ScheduleTest passed: classess - Schedule, SchedulingItem

--- lab10/academiascheduler/Scheduler.cpp
+++ lab10/academiascheduler/Scheduler.cpp
@@ -3,3 +3,83 @@
 //
 
 #include "Scheduler.h"
+#include <algorithm>
+
+academia::Schedule academia::Schedule::OfTeacher(int teacher_id) const {
+    Schedule scheduleofteacher;
+    for(auto i:schedule_){
+        if (i.TeacherId()==teacher_id){
+            scheduleofteacher.InsertScheduleItem(i);
+        }
+    }
+
+/*std::transform(schedule_.begin(), schedule_.end(), std::back_inserter(scheduleofteacher.schedule_),
+               [teacher_id](auto i){return teacher_id==i.TeacherId();});*/
+
+return scheduleofteacher;
+}
+
+academia::Schedule academia::Schedule::OfRoom(int room_id) const {
+    Schedule scheduleofroom;
+    for(auto i:schedule_){
+        if (i.RoomId()==room_id){
+            scheduleofroom.InsertScheduleItem(i);
+        }
+}
+
+return scheduleofroom;
+}
+
+academia::Schedule academia::Schedule::OfYear(int year) const {
+    Schedule scheduleofyear;
+    for(auto i:schedule_){
+        if (i.Year()==year){
+            scheduleofyear.InsertScheduleItem(i);
+    }
+}
+
+return scheduleofyear;
+}
+
+std::vector<int> academia::Schedule::AvailableTimeSlots(int n_time_slots) const {
+    std::vector<int> takenslots;
+    std::vector<int> freeslots;
+
+    for(auto i:schedule_){
+        takenslots.emplace_back(i.TimeSlot());
+    }
+
+    std::sort(takenslots.begin(),takenslots.end());
+    auto last = std::unique(takenslots.begin(), takenslots.end());
+    takenslots.erase(last,takenslots.end());
+
+    int k=0;
+
+    for (int j=1,k=0;j<=n_time_slots;j++){
+        if(k<takenslots.size() && takenslots[k]==j){
+            ++k;
+        }
+        else{
+            freeslots.emplace_back(j);
+        }
+    }
+
+    return freeslots;
+
+
+
+
+
+}
+
+void academia::Schedule::InsertScheduleItem(const academia::SchedulingItem &item) {
+    schedule_.emplace_back(item);
+}
+
+size_t academia::Schedule::Size() const {
+    return schedule_.size();
+}
+
+academia::SchedulingItem academia::Schedule::operator[](int number) const {
+    return schedule_[number];
+}
--- lab10/academiascheduler/Scheduler.h
+++ lab10/academiascheduler/Scheduler.h
@@ -5,10 +5,71 @@
 #ifndef JIMP_EXERCISES_SCHEDULER_H
 #define JIMP_EXERCISES_SCHEDULER_H
 
+#include <vector>
+#include <set>
+#include <map>
+namespace academia{
 
-class Scheduler {
+    class SchedulingItem{ // item from the schedule
+    public:
+        SchedulingItem(int course_id,int teacher_id, int room_id, int time_slot, int year): course_id_(course_id),
+                                                                                            teacher_id_(teacher_id),
+                                                                                            room_id_(room_id),
+                                                                                            time_slot_(time_slot),
+                                                                                            year_(year){}
 
-};
+        //checking methods
+        int CourseId() const{ return course_id_;}
+        int TeacherId() const{ return teacher_id_;}
+        int RoomId() const{ return room_id_;}
+        int TimeSlot() const{ return time_slot_;}
+        int Year() const { return year_;}
+
+
+    private:
+        int course_id_;
+        int teacher_id_;
+        int room_id_;
+        int time_slot_;
+        int year_;
+
+    };
+
+    class Schedule{
+    public:
+        Schedule()= default;
+        // count the chosen part of the schedule
+        Schedule OfTeacher(int teacher_id) const;
+        Schedule OfRoom(int room_id) const;
+        Schedule OfYear(int year) const;
+        std::vector<int> AvailableTimeSlots(int n_time_slots) const;
+
+        //insert item
+        void InsertScheduleItem(const SchedulingItem &item);
+
+        //checking methods
+        size_t Size() const;
+        SchedulingItem operator[](int number) const;
+
+    private:
+        std::vector <SchedulingItem> schedule_;
+
+
+
+    };
+
+
+    class Scheduler{
+    public:
+        Schedule PrepareNewSchedule(const std::vector<int> &rooms,
+                                    const std::map<int, std::vector<int>> &teacher_courses_assignment,
+                                    const std::map<int, std::set<int>> &courses_of_year,
+                                    int n_time_slots);
+
+    };
+
+    //GreedyScheduler //algorithm
+}
 
 
 #endif //JIMP_EXERCISES_SCHEDULER_H
