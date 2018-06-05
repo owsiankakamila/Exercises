abd92f6a50e2fe4c7f9593b4baa2e8b4fdeab0e8
Kamila <owsiankakamila@gmail.com>
2018-06-05 14:50:37 +0200

    Tree(BST) Insert+Find; TreeIterator directory

---
+++ lab12/treeiterators/TreeIterators.h
@@ -0,0 +1,17 @@
+//
+// Created by kamila on 04.06.18.
+//
+
+#ifndef JIMP_EXERCISES_TREEITERATORS_H
+#define JIMP_EXERCISES_TREEITERATORS_H
+
+#include <Tree.h>
+
+namespace tree{
+
+
+}
+
+
+
+#endif //JIMP_EXERCISES_TREEITERATORS_H
